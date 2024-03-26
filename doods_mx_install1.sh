20c02d2065f6dfe08464e00305c4fb1565883f02
Doods Tecson <emmanuel.tecson@gmail.com>
2024-03-26 17:57:58 +0800

    Update and rename doods_mx_install.sh to doods_mx_install1.sh

---
+++ doods_mx_install1.sh
@@ -0,0 +1,29 @@
+#update
+sudo apt update -y && sudo apt upgrade -y
+
+# debloat mx
+# sudo apt purge -y simple-scan transmission-qt featherpad galculator gtkhash gxkb mc mcedit xfburn yad lbreakout2 gnome-mahjongg peg-e swell-foop gmtp strawberry && \
+
+#install apps in official repos
+sudo apt install -y \
+gnome-calculator
+kdeconnect \
+gnome-control-center \
+gnome-online-accounts \
+xfce4-panel-profiles && \
+
+#spotify repo correction
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6224F9941A8AA6D1 && \
+sudo apt-get update -y && \
+
+#update
+sudo apt update -y && sudo apt upgrade -y && \
+
+#install apps not in official repos
+#timeshift-autosnap
+sudo apt install -y git make && \
+git clone https://github.com/wmutschl/timeshift-autosnap-apt.git /home/$USER/timeshift-autosnap-apt
+cd /home/$USER/timeshift-autosnap-apt
+yes | sudo make install && \
+cd && sudo reboot
+
